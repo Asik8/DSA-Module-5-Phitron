03c85b66a0b37bfbbefd376db6d3d32687eac126
Asik8 <motasembillah84@gmail.com>
2024-06-23 06:04:20 +0600

    Constructor creation done

---
+++ Node_DS_With_Constructor.cpp
@@ -0,0 +1,35 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+#define pb push_back
+#define pob pop_back
+
+class Node // initialize Node
+{
+    public:
+    int data;
+    Node *next;
+
+    // Type-1:
+    // Node (int v)     // Constructor
+    // {
+    //     data = v;
+    //     next = NULL;
+    // }
+
+    // Type-2:
+    Node (int data)     // Constructor
+    {
+        this->data = data;
+        next = NULL;
+    }
+};
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(NULL);
+    Node a(5);
+    cout<<a.data<<endl;
+
+    return 0;
+}
